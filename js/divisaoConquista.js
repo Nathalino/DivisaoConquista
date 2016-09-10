2c4117745a3c8c2c7a6e54d8bd1d5e95e666dbff
Nathalino Lenovo <nathalino@outlook.com>
2016-09-10 09:17:11 -0300

    Versão 3

--- js/divisaoConquista.js
+++ js/divisaoConquista.js
@@ -1,61 +1,83 @@
 
+var i = 1;
 var cont = 0;
 var x = [];
 
-function corpo(){
+function corpo(NomeCampo){
 
-    for(var i = 0; i<=400; i++){
+	//Capturando todos os valores digitados no campo Input do html:
+    for(var i = 0; i<=1000; i++){
 
-        if(document.getElementById("field"+i) == null || 
-            document.getElementById("field"+i).value == null || 
-             document.getElementById("field"+i).value == ""){
+        if(document.getElementById(NomeCampo+i) == null || 
+            document.getElementById(NomeCampo+i).value == null || 
+             document.getElementById(NomeCampo+i).value == ""){
             break;
         }else{
-            x[cont] = parseInt(document.getElementById("field"+i).value);
+            x[cont] = parseInt(document.getElementById(NomeCampo+i).value);
             cont++;
         }
     }
 
+    //Tornando a tag com o ID visível:
     document.getElementById("Resultado").style.display = 'block';
 
+    //Inserindo o conteúdo do Array na tag do ID:
+    document.getElementById("numerosInserido").innerHTML = x;
 
-    //console.log(mergeSort(x));
-}
-
-function mergeSort(arr){
-
-    if (arr.length < 2)
-        return arr;
- 
-    var middle = parseInt(arr.length / 2);
-    var left   = arr.slice(0, middle);
-    var right  = arr.slice(middle, arr.length);
- 
-    return merge(console.log(mergeSort(left)), console.log(mergeSort(right));
+    //Inserindo o conteúdo da Função na tag do ID:
+    document.getElementById("numerosOrdenado").innerHTML = mergeSort(x);
 
 }
+
+function mergeSort(vetor){
+
+    if (vetor.length < 2)
+        return vetor;
  
-function merge(left, right){
+    var meio = parseInt(vetor.length / 2);
+    var esquerda   = vetor.slice(0, meio);
+    var direita  = vetor.slice(meio, vetor.length);
+ 
+    inserirElemento(i, esquerda, direita);
+
+    i++;
+
+    return merge(mergeSort(esquerda), mergeSort(direita));
+}
+ 
+function merge(esquerda, direita){
 
     var result = [];
  
-    while (left.length && right.length) {
+    while (esquerda.length != 0 && direita.length != 0) {
 
-        if (left[0] <= right[0]) {
-            result.push(left.shift());
+        if (esquerda[0] <= direita[0]) {
+            result.push(esquerda.shift());
         } else {
-            result.push(right.shift());
+            result.push(direita.shift());
         }
 
     }
  
-    while (left.length)
-        result.push(left.shift());
+    while (esquerda.length)
+        result.push(esquerda.shift());
  
-    while (right.length)
-        result.push(right.shift());
- 
+    while (direita.length)
+        result.push(direita.shift());
+ 	
     return result;
 
 }
 
+function inserirElemento(i, esquerda, direita){
+	//Seleciona a tag do ID:
+	var elemento = document.querySelector('#numerosPartes');
+	//Elemento a ser inserido:
+	var elementoInserir = "<tr>"+
+						  "<td>"+i+"</td>"+
+						  "<td>"+esquerda+"</td>"+
+						  "<td>"+direita+"</td>"+
+						  "</tr>";
+	//Adicionando o elemento na tag do ID selecionado:
+	elemento.insertAdjacentHTML('beforebegin', elementoInserir);
+}
